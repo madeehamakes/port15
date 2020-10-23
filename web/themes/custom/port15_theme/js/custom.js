106344ed5859b5b9341fce410e48c538c2094a33
Ashraf Abed <ashraf.abed@debugacademy.com>
2020-10-23 03:37:25 -0400

    drpl3mo-13: Created port15 subtheme.

---
+++ web/themes/custom/port15_theme/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.port15_theme = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
